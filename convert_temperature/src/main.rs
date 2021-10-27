0c7e2c65b2fc080898954c5b5ca82d7ae1ff93ce
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-27 17:19:54 +0900

    WIP: Create convert temperature

--- convert_temperature/src/main.rs
+++ convert_temperature/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Convert celsius to fahrenheit? y/n");
+
+        let mut is_convert_fahrenheit = String::new();
+        io::stdin().read_line(&mut is_convert_fahrenheit).expect("Failed to read line");
+
+        match is_convert_fahrenheit.trim() {
+            "y" => {
+                println!("Please input celsius");
+                let mut celsius = String::new();
+                io::stdin().read_line(&mut celsius).expect("Failed to read line");
+
+                let fahrenheit: f64 = (celsius.trim().parse().unwrap() * 1.8) + 32;
+                println!("{}", fahrenheit);
+            },
+            "n" => println!("no"),
+            _ => {
+                println!("Please input y or n");
+                continue;
+            },
+        };
+    }
 }
+
+// 摂氏（℃）＝【華氏（℉）-32】÷1.8.
+// 華氏（℉）＝摂氏（℃）×1.8+32.
