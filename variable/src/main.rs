a4a159b36e633a206055403ff5a064e7cda7de00
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-27 10:26:28 +0900

    ADD: Add sample of shadowing

--- variable/src/main.rs
+++ variable/src/main.rs
@@ -4,4 +4,10 @@ fn main() {
     println!("The value of x is {}", x);
     x = 6;
     println!("The value of x is {}", x);
+
+    /* Shadowing */
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is {}", y);
 }
