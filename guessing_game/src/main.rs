f90f0b3f2821333e3a3f19a3f8eaad9b5df5ba0c
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-27 10:42:38 +0900

    REFACTOR: If input not a number, print caution

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,7 +17,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("{} is not a number", guess.trim());
+                continue;
+            },
         };
 
         println!("You guessed: {}", guess);
