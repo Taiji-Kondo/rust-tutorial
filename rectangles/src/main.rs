3dac0749039c2bdad8cfb5e960414280f3290be6
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-29 09:36:16 +0900

    REFACTOR: Refactor function of area

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect = Rectangle {
         width: 30,
@@ -13,10 +19,6 @@ fn main() {
     println!("Rectangle is {:?}", rect);
     println!(
         "The area of the rectangle is {} square pixels",
-        area(&rect)
+        rect.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
