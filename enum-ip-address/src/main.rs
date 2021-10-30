63a91486f8a5e058df3e8d6dcfc319c805c61d98
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-30 18:53:57 +0900

    REFACTOR: Change IpAddrKind to IpAddr

--- enum-ip-address/src/main.rs
+++ enum-ip-address/src/main.rs
@@ -1,8 +1,9 @@
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    println!("Hello, world!");
+    let home = IpAddr::V4(127, 0, 0 ,1);
+    let loopback = IpAddr::V6(String::from("::1"));
 }
