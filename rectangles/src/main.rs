f8b53797d1c4ab199d6563804784050824e94569
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-30 18:13:23 +0900

    ADD: Add function of can_hold

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -8,6 +8,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    // fn square(size: u32) -> Rectangle {
+    //     Rectangle {
+    //         width: size,
+    //         height: size,
+    //     }
+    // }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -15,10 +26,22 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    // let square = Rectangle::square(10);
 
     println!("Rectangle is {:?}", rect);
     println!(
         "The area of the rectangle is {} square pixels",
         rect.area()
     );
+    println!("Can react hold rect2? {}", rect.can_hold(&rect2));
+    println!("Can react hold rect3? {}", rect.can_hold(&rect3));
+    // println!("The area of the square rectangle is {}", square.area());
 }
