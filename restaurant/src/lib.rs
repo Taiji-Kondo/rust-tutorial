7be8fe6a9a60f9aeeb494238de6804c62f26c68e
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-31 14:21:46 +0900

    ADD: Use use modules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -38,12 +38,15 @@ mod back_of_house {
     }
 }
 
+
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     let mut meal = back_of_house::Breakfast::summer("Rye");
 
