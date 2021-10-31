c217f23f39952762ed82fcdeef7439d9577938e4
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-31 17:36:37 +0900

    REFACTOR: Split front_of_house file

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,18 +1,5 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
+pub use crate::front_of_house::hosting;
 
 fn serve_order() {}
 
