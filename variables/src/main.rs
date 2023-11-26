faa8b405a5c8f859c967c3056b3e0ecd227d8cb0
Taiji-Kondo <k.taiji1108@gmail.com>
2023-11-26 22:15:07 +0900

    mutable value and immutable value

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 5; // immutable
+    let mut x = 5; // mutable
+    println!("The value of x is: {}", x);
+    x = 6; // if x is immutable, this will cause an error
+    println!("The value of x is: {}", x);
 }
