acf78f75d2a7552ac599982a61d585c734ded8c1
Taiji-Kondo <k.taiji1108@gmail.com>
2023-11-26 22:21:52 +0900

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,21 @@
+// Mutable and immutable variables
+// fn main() {
+//     // let x = 5; // immutable
+//     let mut x = 5; // mutable
+//     println!("The value of x is: {}", x);
+//     x = 6; // if x is immutable, this will cause an error
+//     println!("The value of x is: {}", x);
+// }
+
+// Shadowing
 fn main() {
-    // let x = 5; // immutable
-    let mut x = 5; // mutable
-    println!("The value of x is: {}", x);
-    x = 6; // if x is immutable, this will cause an error
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
     println!("The value of x is: {}", x);
 }
