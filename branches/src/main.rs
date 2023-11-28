e8bb4b160167928b7441071844fd249b211e0ebc
Taiji-Kondo <k.taiji1108@gmail.com>
2023-11-28 20:18:18 +0900

    if expression

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = String::new();
+
+    println!("Please input a number");
+
+    io::stdin()
+        .read_line(&mut x)
+        .expect("Failed to read line");
+
+    let x: u32 = x.trim()
+        .parse()
+        .expect("Please type a number");
+
+    if x > 5 {
+        println!("x is greater than 5")
+    } else {
+        println!("x is less than 5")
+    }
 }
