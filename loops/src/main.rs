5a295d093184170cd21f2a580c388f541d266761
Taiji-Kondo <k.taiji1108@gmail.com>
2023-11-28 20:37:51 +0900

    for loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
-    // for_loop();
+    simple_loop();
     while_loop();
+    for_loop();
 }
 
-fn for_loop() {
+fn simple_loop() {
     let mut count = 0;
 
     'count_up: loop {
@@ -23,6 +24,13 @@ fn for_loop() {
         count += 1;
     }
     println!("end count: {}", count);
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("the value is: {}", element);
+    }
 }
 
 fn while_loop() {
