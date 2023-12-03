d517cd0523efdf2146edb147516d7cf69d4496ef
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 16:18:20 +0900

    implement associated functions

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -38,15 +38,21 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         other.width < self.width && other.height < self.height
     }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 fn main() {
     let rect1: Rectangle = Rectangle { width: 50, height: 60 };
     let rect2: Rectangle = Rectangle { width: 40, height: 30 };
     let rect3: Rectangle = Rectangle { width: 60, height: 60 };
-    println!("{:?}", rect1);
-    println!("{:?}", rect2);
-    println!("{:?}", rect3);
+    let square: Rectangle = Rectangle::square(100);
+    println!("rect1: {:?}", rect1);
+    println!("rect1: {:?}", rect2);
+    println!("rect1: {:?}", rect3);
+    println!("square: {:?}", square);
 
     // let area = area(&rect);
     let area = rect1.area();
