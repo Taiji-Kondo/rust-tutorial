4468c9cb705272a4a189952e694c07797e69ae1d
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 16:51:04 +0900

    enum sample

--- enum_sample/src/main.rs
+++ enum_sample/src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+
+
+enum IpAddressKind {
+    V4(u32, u32, u32, u32),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", IpAddressKind::V4(100, 255, 255, 255));
+    println!("{:?}", IpAddressKind::V6(String::from("::1")));
 }
