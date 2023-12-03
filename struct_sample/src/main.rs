f0b39dbe6cc735425273b5850762bd99e1564e7b
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 15:49:26 +0900

    area function

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let width: u32 = 5;
+    let high: u32 = 6;
+
+    let area = area(&width, &high);
+    println!("{}", area);
+}
+
+fn area(width: &u32, high: &u32) -> u32 {
+    width * high
 }
