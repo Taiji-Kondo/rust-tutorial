e2a6985c857e6a58f78706c1ab6584620322b779
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 15:53:45 +0900

    area function with tuple

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -1,11 +1,24 @@
-fn main() {
-    let width: u32 = 5;
-    let high: u32 = 6;
+// Normal
+// fn main() {
+//     let width: u32 = 5;
+//     let high: u32 = 6;
+//
+//     let area = area(&width, &high);
+//     println!("{}", area);
+// }
+//
+// fn area(width: &u32, high: &u32) -> u32 {
+//     width * high
+// }
 
-    let area = area(&width, &high);
+// Tuple
+fn main() {
+    let rect: (u32, u32) = (5, 6);
+
+    let area = area(&rect);
     println!("{}", area);
 }
 
-fn area(width: &u32, high: &u32) -> u32 {
+fn area((width, high): &(u32, u32)) -> u32 {
     width * high
 }
