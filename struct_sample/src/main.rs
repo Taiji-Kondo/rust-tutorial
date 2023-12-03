57ff6632bedb4b0165300a642ceea9f04675e358
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-03 15:55:57 +0900

    area function with struct

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -1,24 +1,41 @@
 // Normal
 // fn main() {
 //     let width: u32 = 5;
-//     let high: u32 = 6;
+//     let height: u32 = 6;
 //
-//     let area = area(&width, &high);
+//     let area = area(&width, &height);
 //     println!("{}", area);
 // }
 //
-// fn area(width: &u32, high: &u32) -> u32 {
-//     width * high
+// fn area(width: &u32, height: &u32) -> u32 {
+//     width * height
 // }
 
 // Tuple
+// fn main() {
+//     let rect: (u32, u32) = (5, 6);
+//
+//     let area = area(&rect);
+//     println!("{}", area);
+// }
+//
+// fn area((width, height): &(u32, u32)) -> u32 {
+//     width * height
+// }
+
+// Struct
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect: (u32, u32) = (5, 6);
+    let rect: Rectangle = Rectangle { width: 5, height: 6 };
 
     let area = area(&rect);
     println!("{}", area);
 }
 
-fn area((width, high): &(u32, u32)) -> u32 {
-    width * high
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
